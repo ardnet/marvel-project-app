0b2911fc4fd422475b24441bacb86981db3aec67
Pratomo Ardianto <ardnet@gmail.com>
2020-02-05 16:31:04 +0800

    Manage to display Marvels characters from API to frontend.

--- src/App.js
+++ src/App.js
@@ -1,46 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Contacts from './components/contacts';
 
-function test() {
-  console.log('test hahahh');
+class App extends Component {
+    render() {
+        let results = this.state.contacts.results;
+        let contactsItems = [];
 
-  fetch('https://gateway.marvel.com/v1/public/comics?ts=1&apikey=f973e191e288fda12db9c1c66ef360ed&hash=306c69dd41a3d073cf332c6f8df67a18')
-  .then((response) => {
-    return response.json();
-  })
-  .then((myJson) => {
-    console.log(myJson);
-  });
-  
-  // https://gateway.marvel.com:443/v1/public/characters?apikey=306c69dd41a3d073cf332c6f8df67a18
-  // https://gateway.marvel.com:443/v1/public/characters?apikey=f973e191e288fda12db9c1c66ef360ed
-  // http://gateway.marvel.com/v1/public/comics?ts=1&apikey=f973e191e288fda12db9c1c66ef360ed&hash=306c69dd41a3d073cf332c6f8df67a18
+        for(var result in results){
+          contactsItems.push(<Contacts key={result.toString()} contacts={results[result]} />);
+        }
+        return (
+            <div>
+              <h1>Marvels</h1>
+              {contactsItems}
+            </div>
+        )
+    }
 
+    state = {
+        contacts: []
+    };
 
-}
-
-test();
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    componentDidMount() {
+        fetch('https://gateway.marvel.com/v1/public/comics?ts=1&apikey=f973e191e288fda12db9c1c66ef360ed&hash=306c69dd41a3d073cf332c6f8df67a18')
+            .then(res => res.json())
+            .then((data) => {
+                this.setState({ contacts: data.data })
+            })
+            .catch(console.log)
+    }
 }
 
 export default App;
---
+++ src/components/contacts.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Contacts = ({contacts}) => {
+    return (
+        <div>
+            <div className="card">
+                <div className="card-body">
+                    <h5 className="card-title">{contacts['title']}</h5>
+                    <div className="card-subtitle mb-2 text-muted">
+                      
+                      <img src={contacts['thumbnail'].path + '.' + contacts['thumbnail'].extension} />
+                    </div>
+                    <p className="card-text">{contacts['description']}</p>
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default Contacts
