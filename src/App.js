acde51d30ab989c3945f5ce3639d9d73c60c3642
Pratomo Ardianto <ardnet@gmail.com>
2020-02-06 23:14:25 +0800

    Add new Search component, and restructure code a bit.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,39 @@
 import React, {Component} from 'react';
-import Characters from './components/characters';
+import ListCharacters from './components/listcharacters';
+import SearchCharacters from './components/searchcharacters';
 
 class App extends Component {
-    render() {
-        let results = this.state.characters.results;
-        let charactersItems = [];
-
-        for(var result in results){
-          charactersItems.push(<Characters key={result.toString()} characters={results[result]} />);
-        }
-        return (
-            <div>
-              <h1>Marvel Characters</h1>
-              {charactersItems}
-            </div>
-        )
-    }
-
     state = {
       characters: []
     };
 
     componentDidMount() {
-        fetch('https://gateway.marvel.com/v1/public/comics?ts=1&apikey=f973e191e288fda12db9c1c66ef360ed&hash=306c69dd41a3d073cf332c6f8df67a18')
-            .then(res => res.json())
-            .then((data) => {
-                this.setState({ characters: data.data })
-            })
-            .catch(console.log)
+      fetch('https://gateway.marvel.com/v1/public/characters?ts=1&apikey=f973e191e288fda12db9c1c66ef360ed&hash=306c69dd41a3d073cf332c6f8df67a18')
+        .then(res => res.json())
+        .then((data) => {
+            this.setState({ characters: data.data })
+        })
+        .catch(console.log)
+    }
+
+    render() {
+        let results = this.state.characters.results;
+        let charactersItems = [];
+
+        for(var result in results){
+          charactersItems.push(<ListCharacters key={results[result]['id']} characters={results[result]} />);
+        }
+        return (
+            <div>
+              <h1>Marvel Characters</h1>
+              <div className="search-characters-wrapper">
+                <SearchCharacters />
+              </div>
+              <div className="list-characters-wrapper">
+                {charactersItems}
+                </div>
+            </div>
+        )
     }
 }
 
--- src/components/listcharacters.js
+++ src/components/listcharacters.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Characters = ({characters}) => {
+const ListCharacters = ({characters}) => {
     return (
         <div>
             <div className="card">
                 <div className="card-body">
-                    <h5 className="card-title">{characters['title']}</h5>
+                    <h2 className="card-title">{characters['name']}</h2>
                     <div className="card-subtitle mb-2 text-muted">
                       
                       <img alt="" src={characters['thumbnail'].path + '.' + characters['thumbnail'].extension} className="img-fluid" />
@@ -17,4 +17,4 @@ const Characters = ({characters}) => {
     )
 };
 
-export default Characters
+export default ListCharacters
